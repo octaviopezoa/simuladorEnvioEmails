c6cb34906b5852bbf87fd973259df353cdd1954b
octavio pezoa <octaviopezoa@MacBook-Pro-de-octavio.local>
2022-03-28 23:41:21 -0300

    reordenar codigo y habilitar boton enviar

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ const email = document.querySelector('#email');
 const asunto = document.querySelector('#asunto');
 const mensaje = document.querySelector('#mensaje');
 
+const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 eventListeners();
 function eventListeners() {
@@ -32,7 +33,9 @@ function validarFormulario(e) {
 
         // elimina errores
         const error = document.querySelector('p', '.error');
-        error.remove();
+        if (error) {
+            error.remove();
+        }
 
         e.target.classList.remove('border', 'border-red-500');
         e.target.classList.add('border', 'border-green-500');
@@ -46,11 +49,13 @@ function validarFormulario(e) {
 
     if (e.target.type === "email") {
 
-        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if (er.test(e.target.value)) {
 
-            const error = document.querySelector('p.error');
-            error.remove();
+            const error = document.querySelector('p', '.error');
+
+            if (error) {
+                error.remove();
+            }
 
             e.target.classList.remove('border', 'border-red-500');
             e.target.classList.add('border', 'border-green-500');
@@ -61,6 +66,14 @@ function validarFormulario(e) {
             mostrarError("email no valido");
         }
     }
+
+    // validar formulario
+    if (er.test(email.value) && asunto.value !== "" && mensaje.value !== "") {
+        btnEnviar.disabled = false;
+        btnEnviar.classList.remove('curso-not-allowed', 'opacity-50')
+    }
+
+
 }
 
 function mostrarError(mensaje) {
