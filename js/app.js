18c53332180579d29387b9394f07b00855022558
octavio pezoa <octaviopezoa@MacBook-Pro-de-octavio.local>
2022-03-28 23:07:27 -0300

    agregar mensajes de error

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // variables
 const btnEnviar = document.querySelector('#enviar');
+const formulario = document.querySelector('#enviar-mail');
 
 // variables para campos
 const email = document.querySelector('#email');
@@ -30,5 +31,17 @@ function validarFormulario(e) {
         console.log("hay algo");
     } else {
         e.target.classList.add('border', 'border-red-500');
+        mostrarError();
+    }
+}
+
+function mostrarError() {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
+
+    const errores = document.querySelectorAll('.error');
+    if (errores.length === 0) {
+        formulario.appendChild(mensajeError, document.querySelector('.mb-10'))
     }
 }
